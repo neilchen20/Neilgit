62e113d54a07b81329136dd511ee232a6a1061d8
Neil Chen <neilchnn@gmail.com>
2026-08-20 21:11:56 +0800

    chore: 移除舊版 nlcn 專案

--- nlcn/src/views/notes/vuejs.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <div>
-    <span class="w-50 h-50 bg-dark-50 text-lg">我是vue</span>
-  </div>
-</template>
--- nlcn/vue.config.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  publicPath: import.meta.env.VITE_APP_BASE_URL || '/',
-  filenameHashing: false,
-};
