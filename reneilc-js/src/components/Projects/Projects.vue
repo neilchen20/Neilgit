fc521d51ecaf4cfa054103f84d5883d32b94bbb2
Neil Chen <neilchnn@gmail.com>
2026-08-20 17:05:01 +0800

    fix: 調整 All Projects 寬度

--- reneilc-js/src/components/Projects/Projects.vue
+++ reneilc-js/src/components/Projects/Projects.vue
@@ -78,6 +78,7 @@ const sortedProjects = computed(() => {
       padding: 50px;
       list-style: none;
       text-align: left;
+      max-width: 100%;
       @media (max-width: 991px) {
         max-width: 98%;
         padding: 24px;
